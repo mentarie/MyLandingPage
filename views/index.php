55e4495eae88016e8ecc9fc527dd913e4c0a6384
Mentari E. Rizki <mentari.e.r@mail.ugm.ac.id>
2019-04-28 21:19:30 +0700

    Push Index
    framework : CI
    assets without svgs and webfonts (for font awesome )

---
+++ views/index.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>AII | INDEX</title>
+	<!-- core CSS -->
+    <link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/animate.min.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/owl.carousel.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/owl.transitions.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/prettyPhoto.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/main.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/responsive.css" rel="stylesheet">
+    <link href="<?php echo base_url(); ?>assets/css/custom.css" rel="stylesheet">
+
+    <!-- DataTables -->
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/dataTables.bootstrap.min.css">
+
+    <!-- FONTAWESOME CSS -->
+    <link href="<?php echo base_url(); ?>assets/css/all.css" rel="stylesheet">
+    <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"> -->
+    
+    <!-- FONTAWESOME JS -->
+    <script src="<?php echo base_url(); ?>assets/js/all.js"></script>
+
+    <!-- [if lt IE 9]> -->
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <!-- <![endif]       -->
+
+    <!-- ICON HEAD -->
+    <link rel="shortcut icon" href="<?php echo base_url(); ?>assets/images/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo base_url(); ?>assets/images/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo base_url(); ?>assets/images/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo base_url(); ?>assets/images/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="<?php echo base_url(); ?>assets/images/ico/apple-touch-icon-57-precomposed.png">
+</head>
+<body>
+    <!-- NAV -->
+    <nav class="navbar navbar-lg sticky-top" style="background-color: white">
+        <div class="section-header">
+            <h2 class="section-title text-center wow fadeInDown">WHAT ARE YOU LOOKING FOR?</h2>
+        </div>
+    </nav>
+
+    <div class="container">
+        <p class="text-center wow fadeInDown">Index berisi website yang dikerjakan oleh Mentari E Rizki a.k.a Aii.</p>
+        <div class="row">
+            <!-- PROFILE -->
+            <div class="col-sm-6 wow fadeInLeft">
+                <div class="imov-container">
+                    <img class="img-responsive imov-image" src="<?php echo base_url(); ?>assets/images/main-feature.png">
+                    <div class="imov-middle myBtn">
+                        <div class="imov-text"><a href="websites/uts" target="_blank" style="color: white">See My Profile</a></div>
+                    </div>    
+                </div>
+            </div>
+            <!-- TABLES -->
+            <div class="col-sm-6 wow">
+                <div class="box">
+                    <div class="box-body">
+                      <table id="example1" class="table table-bordered table-hover dataTable">
+                        <thead>
+                        <tr>
+                          <th>Action</th>
+                          <th>Description</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <td>
+                                <div style="margin: 15px;">
+                                    <a href="websites/interstellar" target="_blank">
+                                        <div class="pull-left">
+                                            <i class="fas fa-rocket fa-2x"></i>
+                                        </div>
+                                    </a>
+                                </div>
+                            </td>
+                            <td>
+                                <div class="media-body">
+                                    <h4 class="media-heading">INTERSTELLAR</h4>
+                                    <p>Countdown time web of TKJ Stembayo and Stembase student's who is having industry practice in CV. KHS (Quick)</p>
+                                </div>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td>
+                                <div style="margin: 15px;">
+                                    <a href="websites/erp" target="_blank">
+                                        <div class="pull-left">
+                                            <i class="fa fa-users fa-2x"></i>
+                                        </div>
+                                    </a>
+                                </div>
+                            </td>
+                            <td>
+                                <h4 class="media-heading">ERP / ADM PELATIHAN</h4>
+                                <p>Ticket #138005 : Pembuatan Program Administrasi Seksi Pelatihan </p>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td>
+                                <div style="margin: 15px;">
+                                    <a href="websites/adminer" target="_blank">
+                                        <div class="pull-left">
+                                            <i class="fa fa-database fa-2x"></i>
+                                        </div>
+                                    </a>
+                                </div>
+                            </td>
+                            <td>
+                                <h4 class="media-heading">ADMINER</h4>
+                                <p>.... it's all about databases</p>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td>
+                                <div style="margin: 15px;">
+                                <a href="" data-toggle="modal" data-target="#myModal">
+                                    <div class="pull-left">
+                                        <i class="fa fa-cloud fa-2x"></i>
+                                    </div>
+                                </div>
+                                </a>
+                            </td>
+                            <td>
+                                <h4 class="media-heading">CISCO CCNA</h4>
+                                <p>Offline courses of networking in CISCO CCNA Chapter 1 - 3</p>
+                            </td>
+                          </tr>
+                          <tr>
+                            <td>
+                                <div style="margin: 15px;">
+                                    <a href="" data-toggle="modal" data-target="#myModal2">
+                                        <div class="pull-left">
+                                            <i class="fa fa-object-group fa-2x"></i>
+                                        </div>
+                                    </a>
+                                </div>
+                            </td>
+                            <td>
+                                <h4 class="media-heading">Templates</h4>
+                                <p>Template Semua Website</p>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+            </div>
+        </div>
+    </div>    
+    <br>
+    <footer id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    &copy; 2019 Aii
+                </div>
+                <div class="col-sm-6">
+                    <ul class="social-icons">
+                        <li><a href="https://www.facebook.com/Menthary.guardian.Angel" target="_blank"><i class="fab fa-facebook"></i></a></li>
+                        <li><a href="https://id.pinterest.com/skymentari/" target="_blank"><i class="fab fa-pinterest"></i></a></li>
+                        <li><a href="https://www.linkedin.com/in/mentari-e-8b0a5b127/" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+                        <li><a href="https://github.com/mentarie" target="_blank"><i class="fab fa-github"></i></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer><!--/#footer-->
+
+    <script src="<?php echo base_url(); ?>assets/js/jquery.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/owl.carousel.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/mousescroll.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/smoothscroll.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/jquery.prettyPhoto.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/jquery.isotope.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/jquery.inview.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/wow.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/main.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/custom.js"></script>
+
+    <!-- DataTables -->
+    <script src="<?php echo base_url(); ?>assets/js/jquery.dataTables.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/dataTables.bootstrap.min.js"></script>
+
+    <script type="text/javascript">
+          $(function () {
+            $('#example1').DataTable({
+                "pageLength": 4,
+                "ordering"    : false,
+                "lengthChange": false
+                
+            })
+            $('#example2').DataTable({
+              // 'paging'      : true,
+              // 'searching'   : false,
+              // 'info'        : true,
+              // 'autoWidth'   : false
+            })
+          })
+    </script>
+
+</body>
+</html>
---
+++ views/modals.php
@@ -0,0 +1,190 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	
+    <!-- MODUL CISCO -->
+    <div class="modal fade" id="myModal" role="dialog">
+        <div class="modal-dialog">
+        
+          <!-- Modal content-->
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal">&times;</button>
+              <h4 class="modal-title">Course Index</h4>
+              <p>*If the web isn't show, try to open the link through Internet Explorer</p>
+            </div>
+            <div class="modal-body">
+              <table id="example1" class="table table-bordered table-hover dataTable">
+                <thead>
+                    <td style="width: 10%">Action</td>
+                    <td style="width: 90%">Description</td>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <div style="margin: 15px;">
+                                <a href="websites/Discovery1_English" target="_blank">
+                                    <div class="pull-left">
+                                        <i class="fa fa-signal fa-2x"></i>
+                                    </div>
+                                </a>
+                            </div>
+                        </td>
+                        <td>
+                            <h4 class="media-heading">Chapter 1</h4>
+                            <p>Introducing :
+                                <ul>
+                                    <li>a</li>
+                                    <li>b</li>
+                                    <li>c</li>
+                                </ul>
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div style="margin: 15px;">
+                                <a href="websites/Discovery2_English" target="_blank">
+                                    <div class="pull-left">
+                                        <i class="fa fa-signal fa-2x"></i>
+                                    </div>
+                                </a>
+                            </div>
+                        </td>
+                        <td>
+                            <h4 class="media-heading">Chapter 2</h4>
+                            <p>Introducing :
+                                <ul>
+                                    <li>a</li>
+                                    <li>b</li>
+                                    <li>c</li>
+                                </ul>
+                            </p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div style="margin: 15px;">
+                                <a href="websites/Discovery3_English" target="_blank">
+                                    <div class="pull-left">
+                                        <i class="fa fa-signal fa-2x"></i>
+                                    </div>
+                                </a>
+                            </div>
+                        </td>
+                        <td>
+                            <h4 class="media-heading">Chapter 3</h4>
+                            <p>Introducing :
+                                <ul>
+                                    <li>a</li>
+                                    <li>b</li>
+                                    <li>c</li>
+                                </ul>
+                            </p>
+                        </td>
+                    </tr>
+                </tbody>
+              </table>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+          </div>
+          
+        </div>
+    </div>
+    <!-- MODAL CISCO END -->
+    <!-- MODAL TEMPLATES-->
+    <div class="modal fade" id="myModal2" role="dialog">
+        <div class="modal-dialog">
+        
+          <!-- Modal content-->
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal">&times;</button>
+              <h4 class="modal-title">Template Index</h4>
+            </div>
+            <div class="modal-body">
+              <table id="example1" class="table table-bordered table-hover dataTable">
+                <thead>
+                    <td style="width: 10%">Action</td>
+                    <td style="width: 40%">Description</td>
+                    <td style="width: 50%">View</td>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <div style="margin: 15px;">
+                                <a href="websites/adminlte" target="_blank">
+                                    <div class="pull-left">
+                                        <i class="fa fa-images fa-2x"></i>
+                                    </div>
+                                </a>
+                            </div>
+                        </td>
+                        <td>
+                            <h4 class="media-heading">Admin LTE</h4>
+                        </td>
+                        <td rowspan="4">
+                        	
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div style="margin: 15px;">
+                                <a href="websites/template/Lonely" target="_blank">
+                                    <div class="pull-left">
+                                        <i class="fa fa-images fa-2x"></i>
+                                    </div>
+                                </a>
+                            </div>
+                        </td>
+                        <td>
+                            <h4 class="media-heading">Lonely</h4>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div style="margin: 15px;">
+                                <a href="websites/template/Multi" target="_blank">
+                                    <div class="pull-left">
+                                        <i class="fa fa-images fa-2x"></i>
+                                    </div>
+                                </a>
+                            </div>
+                        </td>
+                        <td>
+                            <h4 class="media-heading">Multi</h4>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div style="margin: 15px;">
+                                <a href="websites/template/progress/progress" target="_blank">
+                                    <div class="pull-left">
+                                        <i class="fa fa-images fa-2x"></i>
+                                    </div>
+                                </a>
+                            </div>
+                        </td>
+                        <td>
+                            <h4 class="media-heading">Progress</h4>
+                        </td>
+                    </tr>
+                </tbody>
+              </table>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
+          </div>
+          
+        </div>
+    </div>
+    <!-- MODAL TEMPLATES END-->
+
+</body>
+</html>
